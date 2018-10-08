12a65c932312dbfd90cb87aae8f5f2effa4df8f7
Tyler Ruth <tjruth99@gmail.com>
2018-10-07 20:40:40 -0400

    Added a new tab for character sheets

--- DnD.js
+++ DnD.js
@@ -2,6 +2,20 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 	var rollButton = document.getElementById('rollButton');
+	
+	var DiceTabButton = document.getElementById('DiceTab');
+	var CharacterTabButton = document.getElementById('CharacterTab');
+
+	DiceTabButton.addEventListener('click', function() { 
+		document.getElementById('character').style.display = 'none';
+		document.getElementById('dice').style.display = 'block';
+	});
+	
+	CharacterTabButton.addEventListener('click', function() { 
+		document.getElementById('dice').style.display = 'none';
+		document.getElementById('character').style.display = 'block';
+	});
+
 
 	rollButton.addEventListener('click', function(){
 		var roll_Input = document.getElementById("roll_in").value;
@@ -14,3 +28,14 @@ document.addEventListener('DOMContentLoaded', function() {
 	})
 
 })
+
+function changeTab(tabId){
+	var i, tabs;
+
+	tabs = document.getElementsByClassName('tab');
+	for(i = 0; i < tabs.length; i++) {
+		tabs[i].style.display = "none";
+	}
+
+	document.getElementById(tabId).style.display = "block";
+}
